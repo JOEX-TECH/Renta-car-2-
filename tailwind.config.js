667eba666b9375b1b40b5479fc1d632b3e6707fd
JOEX-TECH <prospektahub@gmail.com>
2026-05-18 20:08:24 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    darkMode: ["class"],
-    content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
+  darkMode: ["class"],
+  content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
   safelist: [
     'opacity-40', 'pointer-events-none', 'grayscale'
   ],
